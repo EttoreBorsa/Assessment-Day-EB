027eb69f20d12d15333ddb202a27e424f0835117
Ettore Borsa <ettoreborsa@outlook.it>
2026-02-23 22:34:44 +0100

    Frontend: Estrae il componente 'SupplierTable.tsx'

---
+++ src/Frontend/src/components/tables/SupplierTable.tsx
@@ -0,0 +1,45 @@
+// Material
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, styled, tableCellClasses } from "@mui/material";
+
+// Types
+import type { SupplierListQuery } from "../../types/supplier";
+
+interface SupplierTableProps {
+  data: SupplierListQuery[];
+}
+
+export default function SupplierTable({ data }: SupplierTableProps) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="Supplier Table">
+        <TableHead>
+          <TableRow>
+            <StyledTableHeadCell>Name</StyledTableHeadCell>
+            <StyledTableHeadCell>Address</StyledTableHeadCell>
+            <StyledTableHeadCell>Email</StyledTableHeadCell>
+            <StyledTableHeadCell>Phone</StyledTableHeadCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.address}</TableCell>
+              <TableCell>{row.email}</TableCell>
+              <TableCell>{row.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.common.white,
+  },
+}));
--- src/Frontend/src/pages/SupplierListPage.tsx
+++ src/Frontend/src/pages/SupplierListPage.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 
 // Material
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, styled, tableCellClasses } from "@mui/material";
+import { Typography } from "@mui/material";
+
+// Components
+import SupplierTable from "../components/tables/SupplierTable";
 
 // Types
 import type { SupplierListQuery } from "../types/supplier";
@@ -21,42 +24,13 @@ export default function SupplierListPage() {
 
   return (
     <>
+      {/* Title page */}
       <Typography variant="h4" sx={{ textAlign: "center", mt: 4, mb: 4 }}>
         Suppliers
       </Typography>
 
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <StyledTableHeadCell>Name</StyledTableHeadCell>
-              <StyledTableHeadCell>Address</StyledTableHeadCell>
-              <StyledTableHeadCell>Email</StyledTableHeadCell>
-              <StyledTableHeadCell>Phone</StyledTableHeadCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {list.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell>{row.name}</TableCell>
-                <TableCell>{row.address}</TableCell>
-                <TableCell>{row.email}</TableCell>
-                <TableCell>{row.phone}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      {/* Table */}
+      <SupplierTable data={list} />
     </>
   );
 }
-
-const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.primary.light,
-    color: theme.palette.common.white,
-  },
-}));
