570a2f5d9f42a45cd83bc32cd46a9b7989c59715
Ettore Borsa <ettoreborsa@outlook.it>
2026-02-24 18:57:45 +0100

    Frontend (style): Implementa font 'Inter'

--- src/Frontend/src/theme/theme.ts
+++ src/Frontend/src/theme/theme.ts
@@ -1,9 +1,18 @@
+// Font
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+
 // Material
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   shape: {
     borderRadius: 8
+  },
+  typography: {
+    fontFamily: "Inter, Helvetica, Arial, sans-serif",
   },
 });
 
