a7527fa289990daaca03cb77bf0803da7cb687da
Ettore Borsa <ettoreborsa@outlook.it>
2026-02-24 19:51:46 +0100

    Frontend (style): Migliora lo style di 'CustomerButtonExport.tsx'

--- src/Frontend/src/components/buttons/CustomerButtonExport.tsx
+++ src/Frontend/src/components/buttons/CustomerButtonExport.tsx
@@ -1,5 +1,6 @@
 // Material
 import { Button } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
 
 // Utils
 import { exportCustomersXML } from "../../utils/exportCustomersXML";
@@ -15,7 +16,9 @@ export default function CustomerButtonExport({ data }: CustomerButtonExportProps
   return (
     <Button
       variant="contained"
+      startIcon={<DownloadIcon />}
       onClick={() => exportCustomersXML(data)}
+      sx={{ height: "40px", fontSize: "13px" }}
     >
       Export XML
     </Button>
